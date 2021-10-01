ee80ded168b5004591109f2006d385621378d687
Minjae <joshuara7235@spiderkim.com>
2021-10-01 09:49:39 +0900

    Component base design -> using material theme

--- angular-toyproject/src/app/app.module.ts
+++ angular-toyproject/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material/material.module'
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HomeComponent } from './component/home.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
