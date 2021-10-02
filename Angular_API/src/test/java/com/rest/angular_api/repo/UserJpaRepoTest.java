920ea87ec9dcd5581b56da554b23c8be7c4955e8
김민재 <joshuara7235@gmail.com>
2021-10-02 15:57:51 +0900

    Test code for jpa repo on this project

--- Angular_API/src/test/java/com/rest/angular_api/repo/UserJpaRepoTest.java
+++ Angular_API/src/test/java/com/rest/angular_api/repo/UserJpaRepoTest.java
@@ -2,6 +2,7 @@ package com.rest.angular_api.repo;
 
 import com.rest.angular_api.entity.User;
 import com.rest.angular_api.repository.UserJpaRepo;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,13 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.swing.text.html.Option;
+import javax.validation.constraints.AssertTrue;
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -27,13 +35,16 @@ public class UserJpaRepoTest {
 
         //given
         userJpaRepo.save(User.builder()
-                        .uid(uid)
-                        .name(name)
-                        .password(passwordEncoder.encode("1234"))
+                    .uid(uid)
+                    .password(passwordEncoder.encode("1234"))
+                    .name(name)
+                    .roles(Collections.singletonList("ROLE_USER"))
                 .build());
         //when
-
+        Optional<User> user = userJpaRepo.findByUid(uid);
         //then
+        assertTrue(user.isPresent());
+        assertEquals(user.get().getName(), name);
     }
 
 }
