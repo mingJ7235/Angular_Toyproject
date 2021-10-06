f12406fbdd73bc6bbfbdaa96dd6330dda9fd7801
Minjae <joshuara7235@spiderkim.com>
2021-10-06 13:59:48 +0900

    Board API design completed -> reply part should be made

--- Angular_API/src/main/java/com/rest/angular_api/advice/ExceptionAdvice.java
+++ Angular_API/src/main/java/com/rest/angular_api/advice/ExceptionAdvice.java
@@ -76,7 +76,21 @@ public class ExceptionAdvice {
     @ResponseStatus (HttpStatus.INTERNAL_SERVER_ERROR)
     protected CommonResult userExistException (HttpServletRequest request, CUserExistException e) {
         return responseService.getFailResult(Integer.valueOf(getMessage("existingUser.code")), getMessage("existingUser.msg"));
+
     }
+
+    @ExceptionHandler (CNotOwnerException.class)
+    @ResponseStatus (HttpStatus.NON_AUTHORITATIVE_INFORMATION)
+    public CommonResult notOwnerException (HttpServletRequest request, CNotOwnerException e) {
+        return responseService.getFailResult(Integer.valueOf(getMessage("notOwner.code")), getMessage("notOwner.msg"));
+    }
+
+    @ExceptionHandler (CResourceNotExistException.class)
+    @ResponseStatus (HttpStatus.NOT_FOUND)
+    public CommonResult resourceNotExistException (HttpServletRequest request, CResourceNotExistException e) {
+        return responseService.getFailResult(Integer.valueOf(getMessage("resourceNotExist.code")), getMessage("resourceNotExist.msg"));
+    }
+
 
     private String getMessage (String code) {
         return getMessage(code, null);
--- Angular_API/src/main/java/com/rest/angular_api/controller/v1/BoardController.java
+++ Angular_API/src/main/java/com/rest/angular_api/controller/v1/BoardController.java
@@ -2,17 +2,23 @@ package com.rest.angular_api.controller.v1;
 
 import com.rest.angular_api.entity.boards.Board;
 import com.rest.angular_api.entity.boards.Post;
+import com.rest.angular_api.model.board.ParamsPost;
+import com.rest.angular_api.model.response.CommonResult;
 import com.rest.angular_api.model.response.ListResult;
 import com.rest.angular_api.model.response.SingleResult;
+import com.rest.angular_api.repository.PostJpaRepo;
 import com.rest.angular_api.service.ResponseService;
 import com.rest.angular_api.service.board.BoardService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Api(tags = {"3. Board"})
 @RequiredArgsConstructor
@@ -34,6 +40,48 @@ public class BoardController {
     public ListResult<Post> posts (@PathVariable String boardName) {
         return responseService.getListResult(boardService.findPosts(boardName));
     }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam (name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
+    })
+    @ApiOperation(value = "게시판 글 작성", notes = "게시판 글 작성 api")
+    @PostMapping(value = "/{boardName}")
+    public SingleResult<Post> post (@PathVariable String boardName, @Valid @ModelAttribute ParamsPost post) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String uid = authentication.getName();
+        return responseService.getSingleResult(boardService.writePost(uid, boardName, post));
+    }
+
+    @ApiOperation(value = "게시판 글 상세", notes = "게시판 글 상세정보 조회 api")
+    @GetMapping(value = "/post/{postId}")
+    public SingleResult<Post> post (@PathVariable long postId) {
+        return responseService.getSingleResult(boardService.getPost(postId));
+    }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam (name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
+    })
+    @ApiOperation(value = "게시판 글 수정", notes = "게시판 글 수정 api")
+    @PutMapping(value = "/post/{postId}")
+    public SingleResult<Post> post (@PathVariable long postId, @Valid @ModelAttribute ParamsPost post) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String uid = authentication.getName();
+        return responseService.getSingleResult(boardService.updatePost(postId, uid, post));
+    }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam (name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
+    })
+    @ApiOperation(value = "게시판 글 삭제", notes = "게시판 글 삭제 api")
+    @DeleteMapping (value = "/post/{postId}")
+    public CommonResult deletePost (@PathVariable long postId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String uid = authentication.getName();
+        boardService.deletePost(postId, uid);
+        return responseService.getSuccessResult();
+    }
+
+
 
 
 
