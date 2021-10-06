5ce1cc05dc90cf3a93c387c91e89969c2d5ad49d
Minjae <joshuara7235@spiderkim.com>
2021-10-06 17:50:12 +0900

    caching annotation -> configuration and user loading method

--- Angular_API/src/main/java/com/rest/angular_api/service/CustomUserDetailService.java
+++ Angular_API/src/main/java/com/rest/angular_api/service/CustomUserDetailService.java
@@ -1,11 +1,21 @@
 package com.rest.angular_api.service;
 
 import com.rest.angular_api.advice.exception.CUserNotFoundException;
+import com.rest.angular_api.config.redis.redisConfig;
 import com.rest.angular_api.repository.UserJpaRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+
+/**
+ * 빈번히 호출되는 Method Cache 처리
+ * - 자주 호출되는 method에 대한 caching 처리가 중요
+ * - CustomUserDetailService의 메소드는 회원정보를 빈번히 호출하기 때문에 caching을 적용하는것이 좋다.
+ * - @Cacheable을 선언하면, 해당 메서드가 호출될 때 캐시가 없을 경우(최초불러질경우) DB에서 가져와 캐시를 생성하고 데이터를 반환한다.
+ * - 캐시가 이미 있는 경우에는 당연히 DB를 거치지 않고 바로 캐시 데이터를 반환한다.
+ */
 
 @RequiredArgsConstructor
 @Service
@@ -13,7 +23,13 @@ public class CustomUserDetailService implements UserDetailsService {
 
     private final UserJpaRepo userJpaRepo;
 
-    @Override
+    /**
+     * value : 저장시 key 값
+     * key : 키 생성시 추가로 덧붙일 파라미터 정보 선언
+     *       여기서는 메서드의 인자인 userPk를 선언한다.
+     * unless = "#result == null" 은 메서드 결과가 null이 아닌경우에만 캐싱하도록 하는 옵션이다.
+     */
+    @Cacheable(value = redisConfig.CacheKey.USER, key = "#userPk", unless = "#result == null")
     public UserDetails loadUserByUsername(String userPk) {
         return userJpaRepo.findById(Long.valueOf(userPk)).orElseThrow(CUserNotFoundException::new);
     }
--- Angular_API/src/main/java/com/rest/angular_api/service/board/BoardService.java
+++ Angular_API/src/main/java/com/rest/angular_api/service/board/BoardService.java
@@ -3,6 +3,7 @@ package com.rest.angular_api.service.board;
 import com.rest.angular_api.advice.exception.CNotOwnerException;
 import com.rest.angular_api.advice.exception.CResourceNotExistException;
 import com.rest.angular_api.advice.exception.CUserNotFoundException;
+import com.rest.angular_api.config.redis.redisConfig;
 import com.rest.angular_api.entity.boards.Board;
 import com.rest.angular_api.entity.boards.Post;
 import com.rest.angular_api.entity.member.User;
@@ -11,11 +12,23 @@ import com.rest.angular_api.repository.BoardJpaRepo;
 import com.rest.angular_api.repository.PostJpaRepo;
 import com.rest.angular_api.repository.UserJpaRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+
+/**
+ * CRUD 메서드에 캐싱처리
+ * - Spring에서는 캐싱을위해서 몇가지 annotation을 제공한다.
+ *
+ * - @Cacheable : 캐시가 존재하면 메서드를 실행하지 않고 캐시된 값이 반환된다.
+ *                캐시가 존재하지 않으면 메서드가 실행되고 리턴되는 데이터가 캐시에 저장된다.
+ * - @CachePut : 캐시에 데이터를 넣거나 수정시 사용된다. 메서드의 리턴값이 캐시에 없으면 저장하고 있을 경우 갱신한다.
+ * - @CacheEvict : 캐시를 삭제한다.
+ * - @Caching : 여러개의 캐시 annotation이 실행되어야할 때 사용된다.
+ */
 
 @Service
 @Transactional
@@ -27,6 +40,7 @@ public class BoardService {
     private final UserJpaRepo userJpaRepo;
 
     //게시판 이름으로 게시판 조회
+    @Cacheable (value = redisConfig.CacheKey.BOARD, key = "#boardName", unless = "#result == null")
     public Board findBoard (String boardName) {
         return Optional.ofNullable(boardJpaRepo.findByName(boardName)).orElseThrow(CResourceNotExistException::new);
     }
