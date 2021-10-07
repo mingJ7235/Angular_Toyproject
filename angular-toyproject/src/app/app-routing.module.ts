64088f6db5cb3dec9ebfa889a04dfbb6ea151460
Minjae <joshuara7235@spiderkim.com>
2021-10-07 17:22:39 +0900

    Api connection succeed for sign in and sign up from DB

--- angular-toyproject/src/app/app-routing.module.ts
+++ angular-toyproject/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home.component';
 import { SigninComponent } from './component/member/signin.component';
+import { SignupComponent } from './component/member/signup.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'signin', component: SigninComponent}
+  {path: 'signin', component: SigninComponent},
+  {path: 'signup', component: SignupComponent}
 ];
 
 @NgModule({
