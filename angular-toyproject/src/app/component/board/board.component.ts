ff4591eb35d8eae8b21ff96c2a7c80ec737302aa
김민재 <joshuara7235@gmail.com>
2021-10-09 21:31:36 +0900

    Board model and board component file -> dermy data made

--- angular-toyproject/src/app/component/board/board.component.ts
+++ angular-toyproject/src/app/component/board/board.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/model/board/Post';
 
 @Component({
   selector: 'app-board',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
+
+  posts: Post[] = [
+      {'postId':3, 'title': '게시판 테스트03', 'author': '유저03', 'content': '테스트03', 'createdAt': new Date(), 'modifiedAt' : new Date(), 'user' : null},
+      {'postId':2, 'title': '게시판 테스트02', 'author': '유저02', 'content': '테스트02', 'createdAt': new Date(), 'modifiedAt' : new Date(), 'user' : null},
+      {'postId':1, 'title': '게시판 테스트01', 'author': '유저01', 'content': '테스트01', 'createdAt': new Date(), 'modifiedAt' : new Date(), 'user' : null}
+  ];
+
+  headerColumns: string[] = ['postId', 'title', 'author', 'createdAt', 'modifiedAt'];
 
   constructor() { }
 
