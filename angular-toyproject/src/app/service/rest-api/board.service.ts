a4a2771226b6e0e51938623f855a234877f5cf7f
김민재 <joshuara7235@gmail.com>
2021-10-11 15:08:37 +0900

    service for board -> api connection

---
+++ angular-toyproject/src/app/service/rest-api/board.service.ts
@@ -0,0 +1,16 @@
+import { ApiValidationService } from './common/api-validation.service';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BoardService {
+
+  constructor(
+    private http: HttpClient,
+    private apiValidationService : ApiValidationService
+  ) { }
+
+  private getBoardUrl = 'api/v1/board'
+}
